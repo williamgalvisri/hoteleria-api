6a6edd68f0e03c8be59681b0e47e4398057a8826
William Galvis <williamgalvisrivera@gmail.com>
2024-01-02 15:13:56 -0500

    update ports app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { EmailClient } from "@azure/communication-email";
 import express from 'express';
 import bodyParser from 'body-parser';
 const app = express()
-const port = 3000;
+const port = 3001;
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
